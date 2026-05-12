fdc8825815c0740213019dd6870a86d6628d1bf8
vaisset <timothee.vaisset@ensta.fr>
2026-05-12 16:36:48 +0200

    debut_metropolis

--- metropolis.c
+++ metropolis.c
@@ -9,3 +9,15 @@
 #include "utils.h"
 #include "model.h"
 
+
+
+
+
+double boltzman_probability(double energy, double T){
+    int i = rand()%model->lat->Lx;
+    int j = rand()%model->lat->Ly;
+    int current_spin = model->lat->spins[i][j];
+
+    int right = 0, left = 0, bottom = 0, top = 0;
+    
+}
