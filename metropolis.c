e0c4dc955e8239ed3780f6eed7241859a4267cce
vaisset <timothee.vaisset@ensta.fr>
2026-05-12 16:41:21 +0200

    includes_metropolis

--- metropolis.c
+++ metropolis.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+#include <stdbool.h>
 
-#include "metropolis.h"
+#include "lattice.h"
+#include "utils.h"
+#include "model.h"
 
 
 double boltzman_probability(double delta_E, double T) {
